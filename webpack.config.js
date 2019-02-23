aa704acd6de38b3de02bd58a39d53c09bbb39678
popov7 <deniolp@gmail.com>
2019-02-23 15:47:21 +0400

    Installing webpack server and making other things for enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const path = require(`path`);
 
 module.exports = {
   mode: `development`,
-  entry: `.src/main.js`,
+  entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  devtool: `source-map`
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080/`,
+    hot: true,
+    compress: true
+  }
 };
