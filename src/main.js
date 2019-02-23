628cb5a3605fea2c53585c7489d9ee8ce37ea3eb
popov7 <deniolp@gmail.com>
2019-02-23 15:13:18 +0400

    Filling 3 containers by cinema cards with different logic

--- src/main.js
+++ src/main.js
@@ -36,26 +36,33 @@ const getFilterElement = (name, hasAmount = true, amount, isActive = false, isAd
   return filterTemplate.content.cloneNode(true);
 };
 
-const getCinemaCard = () => {
-  const cardMarkdown = `
-  <article class="film-card">
-  <h3 class="film-card__title">The Assassination Of Jessie James By The Coward Robert Ford</h3>
-  <p class="film-card__rating">9.8</p>
-  <p class="film-card__info">
-    <span class="film-card__year">2018</span>
-    <span class="film-card__duration">1h&nbsp;13m</span>
-    <span class="film-card__genre">Comedy</span>
-  </p>
-  <img src="./images/posters/three-friends.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">A priest with a haunted past and a novice on the threshold of her final vows are sent by the Vatican to investigate the death of a young nun in Romania and confront a malevolent force in the form of a demonic nun.</p>
-  <button class="film-card__comments">13 comments</button>
-
+const getCinemaCard = (isControls = true) => {
+  const descriptionElement = `
+  <p class="film-card__description">A priests Romania and confront a malevolent force in the form of a demonic nun.</p>
+  `;
+  const formElement = `
   <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist"><!--Add to watchlist--> WL</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched"><!--Mark as watched-->WTCHD</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite"><!--Mark as favorite-->FAV</button>
-  </form>
-</article>
+  <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+  <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+  <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+</form>
+  `;
+
+  const cardMarkdown = `
+  <article class="film-card${isControls ? `` : ` film-card--no-controls`}">
+    <h3 class="film-card__title">Incredibles 2</h3>
+    <p class="film-card__rating">9.8</p>
+    <p class="film-card__info">
+      <span class="film-card__year">2018</span>
+      <span class="film-card__duration">1h&nbsp;13m</span>
+      <span class="film-card__genre">Comedy</span>
+    </p>
+    <img src="./images/posters/moonrise.jpg" alt="" class="film-card__poster">
+    ${isControls ? descriptionElement : ``}
+    <button class="film-card__comments">13 comments</button>
+
+    ${isControls ? formElement : ``}
+  </article>
   `;
 
   const cardTemplate = document.createElement(`template`);
@@ -65,11 +72,19 @@ const getCinemaCard = () => {
 
 const filtersContainer = document.querySelector(`.main-navigation`);
 const cinemaCardsContainer = document.querySelector(`.films-list__container`);
-const topRatedContainer = document.querySelector(`.films-list--extra:first-child .films-list__container`);
-const mostCommentedContainer = document.querySelector(`.films-list--extra:nth-child(2) .films-list__container`);
+const topRatedContainer = document.querySelector(`section.films-list--extra .films-list__container`);
+const mostCommentedContainer = document.querySelector(`section.films-list--extra:last-of-type .films-list__container`);
 
 FILTERS.forEach((item) => filtersContainer.appendChild(getFilterElement(item.name, item.hasAmount, getRandomNumber(), item.isActive, item.isAdditional)));
 
 for (let i = 0; i < 7; i++) {
   cinemaCardsContainer.appendChild(getCinemaCard());
+}
+
+for (let i = 0; i < 2; i++) {
+  topRatedContainer.appendChild(getCinemaCard(false));
+}
+
+for (let i = 0; i < 2; i++) {
+  mostCommentedContainer.appendChild(getCinemaCard(false));
 }
