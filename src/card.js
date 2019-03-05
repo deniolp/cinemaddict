0d4986d777d60107f605901f95189f847227dfc2
popov7 <deniolp@gmail.com>
2019-03-05 16:57:41 +0400

    Creating class Card with it's private fields

---
+++ src/card.js
@@ -0,0 +1,12 @@
+class Card {
+  constructor({title, poster, description, rating, year, duration, genre, commentsQuantity}) {
+    this._title = title;
+    this._poster = poster;
+    this._description = description;
+    this._rating = rating;
+    this._year = year;
+    this._duration = duration;
+    this._genre = genre;
+    this._commentsQuantity = commentsQuantity;
+  }
+}
