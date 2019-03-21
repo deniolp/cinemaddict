76d9931188babbcae2ffe5cb2491af226dc2e5ba
popov7 <deniolp@gmail.com>
2019-03-21 18:16:58 +0300

    Preparing chart

--- src/draw-stat.js
+++ src/draw-stat.js
@@ -1,10 +1,70 @@
 import Chart from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
+const BAR_HEIGHT = 50;
+
 const statisticCtx = document.querySelector(`.statistic__chart`);
+statisticCtx.height = BAR_HEIGHT * 5;
+
+let myChart = null;
 
 const drawStat = (cards) => {
-
+  const myChart = new Chart(statisticCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`Comedy`, `History`, `Drama`, `Horror`, `Series`, `Western`, `Action`, `Adventure`],
+      datasets: [{
+        data: [11, 8, 7, 4, 3, 2, 1, 1],
+        backgroundColor: `#ffe800`,
+        hoverBackgroundColor: `tomato`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 20
+          },
+          color: `#ffffff`,
+          anchor: `start`,
+          align: `start`,
+          offset: 40,
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#ffffff`,
+            padding: 100,
+            fontSize: 20
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 24
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
+  });
 };
 
 export {drawStat};
